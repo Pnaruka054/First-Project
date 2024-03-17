10f7bbd04e5319aa81e7165843b94e507813b9ca
pnaruka054 <pnaruka054@gmail.com>
2024-03-17 16:08:47 +0530

    first commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className='Footer'>
             <div className='FooterTop'>
-                <img src='logo.png' />
+                <img src='logo1.png' />
             </div>
             <div className='FooterBottomDiv'>
                 <p>Morbi convallis bibendum urna ut viverra. Maecenas consequat</p>
