d4074f7a14bd63b2175d8383d74c34f6c778622d
pnaruka054 <pnaruka054@gmail.com>
2024-03-20 07:11:40 +0530

    first commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
                 <a href='./'><img src='./Logo.png' alt='Website Logo' /></a>
             </div>
             <div className='FooterBottomDiv'>
-                <p><i class="fa-solid fa-location-check"></i>Kakriya ki Dhani Toda Meena, Jamwaramgragh, Jaipur, Rajasthan</p>
-                <a href='tel:+91 9351585149'><i class="fa-solid fa-phone"></i>+91 9351585149</a>
-                <a href='mailto:Pinkcityjaipurtourandtravelsra@gmail.com'><i class="fa-solid fa-envelope"></i>Pinkcityjaipurtourandtravelsra@gmail.com</a>
+                <p><i className="fa-solid fa-location-check"></i>Kakriya ki Dhani Toda Meena, Jamwaramgragh, Jaipur, Rajasthan</p>
+                <a href='tel:+91 9351585149'><i className="fa-solid fa-phone"></i>+91 9351585149</a>
+                <a href='mailto:Pinkcityjaipurtourandtravelsra@gmail.com'><i className="fa-solid fa-envelope"></i>Pinkcityjaipurtourandtravelsra@gmail.com</a>
             </div>
             <div className='FooterCopyright'>
                 <p>© 2024 Site Owner, Inc. <a href='./'>All rights reserved.</a></p>
